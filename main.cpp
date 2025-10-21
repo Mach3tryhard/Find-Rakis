e795c058cf394ae417a778cae3c8c8e63cf0ccba
Matei Sirghe <matei.sirghe@gmail.com>
2025-10-21 09:44:14 +0300

    Adaugat codul de la mainul vechi

--- main.cpp
+++ main.cpp
@@ -1,22 +1,166 @@
 #include <iostream>
 #include <array>
+#include <vector>
 #include <chrono>
 #include <thread>
-
 #include <SFML/Graphics.hpp>
 
-//////////////////////////////////////////////////////////////////////
-/// This class is used to test that the memory leak checks work as expected even when using a GUI
-class SomeClass {
-public:
-    explicit SomeClass(int) {}
+struct Pair {
+    double x,y;
 };
+class Collider {};
 
-SomeClass *getC() {
-    return new SomeClass{2};
+class Physics {
+private:
+    Pair position;
+    Pair velocity;
+public:
+    /// CONSTRUCTORS
+    Physics() {}
+    Physics(const Physics& state) {
+        this->position = state.position;
+        this->velocity = state.velocity;
+    }
+    Physics(Pair position, Pair velocity) {
+        this->position = position;
+        this->velocity = velocity;
+    }
+    ~Physics() {}
+    /// OPERATORS
+    Physics& operator=(const Physics& state) {
+        this->position = state.position;
+        this->velocity = state.velocity;
+        return *this;
+    }
+    friend std::ostream& operator<<(std::ostream& out,const Physics& state);
+    /// FUNCTIONS
+    void UpdatePosition() {
+        this->position.x += velocity.x;
+        this->position.y += velocity.y;
+
+    }
+};
+std::ostream& operator<<(std::ostream& out, const Physics& state) {
+    out<<"Position:X-"<<state.position.x<<" Y-"<<state.position.y<<'\n';
+    out<<"Velocity:X-"<<state.velocity.x<<" Y-"<<state.velocity.y<<'\n';
+    return out;
 }
-//////////////////////////////////////////////////////////////////////
 
+class Celestial {
+private:
+    Collider collider;
+    Physics physics;
+    int health;
+    double radius,gravity,gravity_radius;
+    int color;
+    bool solid;
+public:
+    Celestial(Physics physics,Collider collider,int health,double radius,double gravity,double gravity_radius,int color,bool solid){
+        this->collider = collider;
+        this->physics = physics;
+        this->health = health;
+        this->radius = radius;
+        this->gravity = gravity;
+        this->gravity_radius = gravity_radius;
+        this->color = color;
+        this->solid = solid;
+    }
+    friend std::ostream& operator<<(std::ostream& out,Celestial body);
+};
+std::ostream& operator<<(std::ostream& out,Celestial body){
+    out<<"BODY\n";
+    out<<body.physics<<'\n';
+    out<<"Stats:\n";
+    out<<"Health:"<<body.health<<'\n'<<"Radius:"<<body.radius<<'\n';
+    out<<"Gravity:"<<body.gravity<<'\n'<<"Gravity_Radius:"<<body.gravity_radius<<'\n';
+    out<<"Color:"<<body.color<<'\n'<<"Solid:"<<body.solid<<'\n';
+    return out;
+}
+
+class SpaceShip {
+private:
+    Collider collider;
+    Physics physics;
+    double direction;
+    double fuel,energy,ore;
+public:
+    SpaceShip(Physics physics,Collider collider,double direction, double fuel, double energy, double ore) {
+        this->collider = collider;
+        this->physics = physics;
+        this->direction = direction;
+        this->fuel = fuel;
+        this->energy = energy;
+        this->ore = ore;
+    }
+    friend std::ostream& operator<<(std::ostream& out,SpaceShip ship);
+};
+std::ostream& operator<<(std::ostream& out,SpaceShip ship) {
+    out<<"SHIP\n";
+    out<<ship.physics<<'\n';
+    out<<"Stats:\n";
+    out<<"Direction:"<<ship.direction<<'\n';
+    out<<"Fuel:"<<ship.fuel<<"Energy:"<<ship.energy<<"ORE:"<<ship.ore<<'\n';
+    return out;
+}
+
+
+class Bullet {
+private:
+    Collider collider;
+    Physics physics;
+    int damage;
+public:
+    Bullet(Physics physics,Collider collider,int damage) {
+        this->collider = collider;
+        this->physics = physics;
+        this->damage = damage;
+    }
+    friend std::ostream& operator<<(std::ostream& out,Bullet bullet);
+};
+std::ostream& operator<<(std::ostream& out,Bullet bullet) {
+    out<<"BULLET\n";
+    out<<bullet.physics<<'\n';
+    out<<"Stats:\n";
+    out<<"Damage:"<<bullet.damage<<'\n';
+    return out;
+}
+
+class SolarSystem {
+private:
+    Physics physics;
+    std::vector<Celestial> bodies;
+public:
+    SolarSystem(std::vector<Celestial> bodies) {
+        for (auto& bod : bodies) {
+            this->bodies.push_back(bod);
+        }
+    };
+    SolarSystem() {
+        //TODO: insert random planet generation
+    }
+    friend std::ostream& operator<<(std::ostream& out,SolarSystem system);
+};
+std::ostream& operator<<(std::ostream& out,SolarSystem system) {
+    out<<"SOLAR SYSTEM\n";
+    for (auto& bod : system.bodies) {
+        out<<bod<<'\n';
+    }
+    return out;
+}
+
+class Universe {
+private:
+    std::vector<SolarSystem> systems;
+public:
+    Universe(std::vector<SolarSystem> systems) {
+        for (auto& system : systems) {
+            this->systems.push_back(system);
+        }
+    };
+    Universe() {
+        //TODO: Random Systems generation
+    }
+};
 
 int main() {
     ///
@@ -64,10 +208,6 @@ int main() {
     ///     fis >> v2[i];
     ///
     ///////////////////////////////////////////////////////////////////////////
-
-    SomeClass *c = getC();
-    std::cout << c << "\n";
-    delete c;  // comentarea acestui rând ar trebui să ducă la semnalarea unui mem leak
 
     sf::RenderWindow window;
     ///////////////////////////////////////////////////////////////////////////
