993ef79a47db1e2d0a070d76ad6c2ef64a60d1c6
Matei Sirghe <matei.sirghe@gmail.com>
2025-12-02 15:05:37 +0200

    Game manager class added

---
+++ code/GameManager.cpp
@@ -0,0 +1,151 @@
+#include "GameManager.h"
+#include "SpaceShip.h"
+
+void GameManager::resetGame(){
+    sf::Vector2f center = {SCR_WIDTH/2.f, SCR_HEIGHT/2.f};
+
+    player.getPhysics().setPosition({center.x, center.y});
+    player.setLast({center.x, center.y});
+    player.getPhysics().setVelocity({0, 0});
+    player.getPhysics().setAcceleration({0, 0});
+
+    player.setFuel(100);
+    player.setDead(false);
+    player.setEnergy(100);
+    player.setOre(100);
+    player.setTimer(0);
+    player.setDistance_travelled(0);
+    player.setWon(false);
+}
+
+void GameManager::Run() {
+    while(window.isOpen()) {
+        sf::Time dt = clock.restart();
+        while(const std::optional event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+            if (inMenu) {
+                mainMenu.HandleInput(*event, window);
+            }
+            else if (player.getWon()) {
+                winMenu.HandleInput(*event, window);
+            }
+            else if (player.getDead()) {
+                deathMenu.HandleInput(*event, window);
+            }
+            else if (isPaused) {
+                pauseMenu.HandleInput(*event, window);
+                if (event->is<sf::Event::KeyPressed>()) {
+                    if (event->getIf<sf::Event::KeyPressed>()->scancode == sf::Keyboard::Scancode::Escape)
+                        isPaused = false;
+                }
+            }
+            else {
+                if (event->is<sf::Event::Resized>()) {
+                    const auto* resize = event->getIf<sf::Event::Resized>();
+                    float aspect = 1920.f / 1008.f;
+                    float windowAspect = static_cast<float>(resize->size.x) / resize->size.y;
+
+                    float viewWidth, viewHeight;
+                    if (windowAspect >= aspect) {
+                        viewHeight = 1008.f;
+                        viewWidth = 1008.f * windowAspect;
+                    } else {
+                        viewWidth = 1920.f;
+                        viewHeight = 1920.f / windowAspect;
+                    }
+
+                    view.setSize({viewWidth, viewHeight});
+                    sf::Vector2f newCenter = {static_cast<float>(player.getPhysics().getPosition().x),static_cast<float>(player.getPhysics().getPosition().y)};
+                    view.setCenter(newCenter);
+                    sf::FloatRect newRect = {{0.f,0.f}, {1.f,1.f}};
+                    view.setViewport(newRect);
+                    window.setView(view);
+                }
+
+                else if (event->is<sf::Event::KeyPressed>()) {
+                    const auto* keyPressed = event->getIf<sf::Event::KeyPressed>();
+
+                    if(keyPressed->scancode == sf::Keyboard::Scancode::Escape) {
+                        isPaused = true;
+                    }
+                    if(keyPressed->code == sf::Keyboard::Key::C) {
+                        if (player.getEnergy()>10)
+                            player.ShootBullet();
+                    }
+                }
+            }
+        }
+
+        window.clear();
+        if (isPaused || player.getDead() || player.getWon()) {
+            dt = sf::Time::Zero;
+        }
+        if (inMenu) {
+            window.setView(window.getDefaultView());
+            sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+            mainMenu.Update(mousePos);
+            mainMenu.Draw(window);
+            starfield->Update(dt,window,player);
+        }
+        else {
+            player.SetRefuel(false,{0,0},0,sf::Color::White);
+            /// UNIVERSE STUFF
+            starfield->Update(dt,window,player);
+            universe->Update(player,window,viewRect,noise.getTexture(),dt);
+            /// PARTICLE STUFF
+            player.getExhaust().update(dt);
+            window.draw(player.getExhaust());
+            /// DRAW GUI
+            gui.DrawText(window,player);
+            gui.DrawArrowHUD(window, player);
+            gui.DrawMiniMap(window,*universe,player);
+            gui.DrawBars(window,player);
+            if (!player.getDead() && !isPaused && !player.getWon()) {
+                /// PLAYER STUFF
+                player.UpdateData(dt,player.getPhysics().getPosition());
+                player.UpdateBullets(dt,window,viewRect);
+                player.InputCheck(dt, window);
+                player.HyperLogic(dt,window);
+                window.draw(player.getShape());
+            }
+            if (player.getWon()) {
+                window.setView(window.getDefaultView());
+
+                sf::RectangleShape ScreenWIN(sf::Vector2f(window.getSize()));
+                ScreenWIN.setFillColor(sf::Color(200, 200, 200, 150));
+                window.draw(ScreenWIN);
+                player.StopAudio();
+
+                sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                winMenu.Update(mousePos);
+                winMenu.Draw(window);
+            }
+            if (player.getDead()) {
+                window.setView(window.getDefaultView());
+
+                sf::RectangleShape Screen(sf::Vector2f(window.getSize()));
+                Screen.setFillColor(sf::Color(200, 200, 200, 150));
+                window.draw(Screen);
+                player.StopAudio();
+
+                sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                deathMenu.Update(mousePos);
+                deathMenu.Draw(window);
+            }
+            if (isPaused) {
+                window.setView(window.getDefaultView());
+
+                sf::RectangleShape dark(sf::Vector2f(window.getSize()));
+                dark.setFillColor(sf::Color(0,0,0,150));
+                window.draw(dark);
+
+                sf::Vector2f mousePos = window.mapPixelToCoords(sf::Mouse::getPosition(window));
+                pauseMenu.Update(mousePos);
+                pauseMenu.Draw(window);
+                window.draw(player.getShape());
+            }
+        }
+        window.display();
+    }
+}
---
+++ code/GameManager.h
@@ -0,0 +1,145 @@
+#ifndef OOP_GAMEMANAGER_H
+#define OOP_GAMEMANAGER_H
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <SFML/Graphics.hpp>
+#include "Pair.h"
+#include "Physics.h"
+#include "Bullet.h"
+#include "Celestial.h"
+#include "Exceptions.h"
+#include "Generator.h"
+#include "GUI.h"
+#include "ParticleSystem.h"
+#include "Universe.h"
+#include "Generator.h"
+#include "Starfield.h"
+#include "Menu.h"
+
+class GameManager {
+private:
+    const float SCR_WIDTH = 1920;
+    const float SCR_HEIGHT = 1008;
+
+    sf::RenderWindow window;
+    sf::View view;
+    sf::Vector2f center;
+    sf::Vector2f size;
+    sf::FloatRect viewRect;
+    std::mt19937 gen;
+
+    GUI gui{};
+
+    Physics playerphysics{5};
+    SpaceShip player;
+    Starfield*starfield = nullptr;
+    Universe*universe = nullptr;
+    Generator noise;
+
+    bool inMenu = true;
+    bool isPaused = false;
+
+    Menu mainMenu;
+    Menu pauseMenu;
+    Menu deathMenu;
+    Menu winMenu;
+
+    sf::Clock clock;
+public:
+    void resetGame();
+    void Run();
+
+    GameManager() : player(playerphysics,10,100,100,100), noise(1000,sf::Color::White,0), mainMenu(SCR_WIDTH, SCR_HEIGHT,"FIND RAKIS"),
+                    pauseMenu(SCR_WIDTH, SCR_HEIGHT,"PAUSED"), deathMenu(SCR_WIDTH, SCR_HEIGHT,"YOU DIED"),
+                    winMenu(SCR_WIDTH, SCR_HEIGHT,"YOU WIN") {
+
+        window.create(sf::VideoMode({1920, 1008}), "Find Rakis", sf::Style::Default);
+        window.setVerticalSyncEnabled(true);
+        view=sf::View(sf::FloatRect({0, 0}, {1920, 1008}));
+        window.setView(view);
+
+        center = view.getCenter();
+        size = view.getSize();
+        viewRect=sf::FloatRect({center.x - size.x/2.f, center.y - size.y/2.f}, {size.x, size.y});
+        gen=std::mt19937(std::random_device{}());
+
+        gui.Initialize(window);
+
+        player.getShape().setPosition({center.x,center.y});
+
+        starfield = new Starfield(250, window.getSize());
+        universe = new Universe(25, gen);
+
+        mainMenu.AddButton<bool>("PLAY", 450.f, &inMenu, [](bool* val) {
+            *val = false;
+        });
+        mainMenu.AddButton<bool>("REGENERATE", 550.f, &isPaused, [this](bool* val){
+            delete universe;
+            delete starfield;
+
+            universe = new Universe(25, gen);
+            starfield = new Starfield(250, window.getSize());
+
+            this->resetGame();
+
+            *val = false;
+        });
+        mainMenu.AddButton<sf::RenderWindow>("EXIT", 650.f, &window, [](sf::RenderWindow* win) {
+            win->close();
+        });
+
+        pauseMenu.AddButton<bool>("RESUME", 450.f, &isPaused, [](bool* val) {
+           *val = false;
+       });
+        pauseMenu.AddButton<bool>("QUIT", 550.f, &inMenu, [this](bool* val){
+            *val = true;
+            isPaused = false;
+            this->resetGame();
+        });
+
+        deathMenu.AddButton<SpaceShip>("PERSIST", 450.f, &player, [this](SpaceShip* p) {
+            this->resetGame();
+            delete universe;
+            delete starfield;
+
+            universe = new Universe(25, gen);
+            starfield = new Starfield(250, window.getSize());
+        });
+        deathMenu.AddButton<bool>("EXIT", 550.f, &inMenu, [this](bool* val) {
+            *val = true;
+            this->resetGame();
+            delete universe;
+            delete starfield;
+
+            universe = new Universe(25, gen);
+            starfield = new Starfield(250, window.getSize());
+        });
+
+        winMenu.AddButton<SpaceShip>("RESTART", 450.f, &player, [this](SpaceShip* p) {
+            this->resetGame();
+            delete universe;
+            delete starfield;
+
+            universe = new Universe(25, gen);
+            starfield = new Starfield(250, window.getSize());
+        });
+        winMenu.AddButton<bool>("EXIT", 550.f, &inMenu, [this](bool* val) {
+            *val = true;
+            this->resetGame();
+            delete universe;
+            delete starfield;
+
+            universe = new Universe(25, gen);
+            starfield = new Starfield(250, window.getSize());
+        });
+    }
+    ~GameManager() {
+        delete universe;
+        delete starfield;
+    }
+};
+
+
+#endif //OOP_GAMEMANAGER_H
